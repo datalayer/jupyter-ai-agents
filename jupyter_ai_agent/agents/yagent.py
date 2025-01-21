aeec4fd650c147d8c8deed2a9a166a73c2f51f64
Frédéric Collonval <fcollonval@users.noreply.github.com>
2025-01-21 08:55:29 +0100

    Continue agent implementation

--- jupyter_ai_agent/agents/yagent.py
+++ jupyter_ai_agent/agents/yagent.py
@@ -8,12 +8,14 @@ from __future__ import annotations
 
 from enum import IntEnum
 from typing import Any, Literal, cast
-from pycrdt import MapEvent, ArrayEvent
 
 from jupyter_nbmodel_client import NbModelClient
+from pycrdt import Map
 
 
 class AIMessageType(IntEnum):
+    ACKNOWLEDGE = 0
+    """Prompt is being processed."""
     SUGGESTION = 1
     """Message suggesting a new cell content."""
     EXPLANATION = 2
@@ -21,8 +23,10 @@ class AIMessageType(IntEnum):
 
 
 class BaseYNotebookAgent(NbModelClient):
+    # FIXME implement username retrieval
+
     def _on_notebook_changes(
-        self, part: Literal["state"] | Literal["meta"] | Literal["cells"], changes: Any
+        self, part: Literal["state"] | Literal["meta"] | Literal["cells"] | str, changes: Any
     ) -> None:
         if part == "cells":
             path_length = len(changes.path)
@@ -47,9 +51,41 @@ class BaseYNotebookAgent(NbModelClient):
                                 changes.target["id"], change.get("newValue"), change["oldValue"]
                             )
                     elif key == "metadata":
-                        ...
-                    elif key == "outputs":
-                        ...
+                        new_metadata = change.get("newValue", {})
+                        datalayer_ia = new_metadata.get("datalayer", {}).get("ai", {})
+                        prompts = datalayer_ia.get("prompts", [])
+                        prompt_ids = {prompt["id"] for prompt in prompts}
+                        new_prompts = prompt_ids.difference(
+                            message["parent_id"] for message in datalayer_ia.get("messages", [])
+                        )
+                        if new_prompts and change["action"] in {"add", "update"}:
+                            for prompt in filter(lambda p: p.get("id"), prompts):
+                                self._on_user_prompt(changes.target["id"], prompt)
+                        # elif change["action"] == "delete":
+                        #     ...
+                    # elif key == "outputs":
+                    #     # TODO
+                    #     ...
+            elif (
+                path_length == 2
+                and isinstance(changes.path[0], int)
+                and changes.path[1] == "metadata"
+            ):
+                # Change in cell metadata
+                for key, change in changes.keys.items():
+                    if key != "datalayer":
+                        new_metadata = change.get("newValue", {})
+                        datalayer_ia = new_metadata.get("ai", {})
+                        prompt = datalayer_ia.get("prompt")
+                        messages = datalayer_ia.get("messages")
+                        if prompt and not messages and change["action"] in {"add", "update"}:
+                            self._on_user_prompt(changes.target["id"], prompt)
+                        # elif change["action"] == "delete":
+                        #     ...
+
+        # elif part == "meta":
+        #     # FIXME handle notebook metadata
+
         # print(f"{part}")
 
         # def print_change(changes):
@@ -71,42 +107,76 @@ class BaseYNotebookAgent(NbModelClient):
         super()._reset_y_model()
         self._doc.observe(self._on_notebook_changes)
 
-    def _on_user_prompt(self, cell_id: str, prompt: str) -> None: ...
+    def _on_user_prompt(self, cell_id: str, prompt: str, username: str | None = None) -> None:
+        username = username or self._username
+        self._log.debug("New AI prompt sets by user [%s] in [%s]: [%s].", username, cell_id, prompt)
 
-    def _on_cell_source_changes(self, cell_id: str, new_source: str, old_source: str) -> None: ...
+    def _on_cell_source_changes(
+        self, cell_id: str, new_source: str, old_source: str, username: str | None = None
+    ) -> None:
+        username = username or self._username
+        self._log.debug("New cell source sets by user [%s] in [%s].", username, cell_id, new_source)
 
-    def _on_cell_outputs_changes(self, *args) -> None:
-        print(args)
+    # def _on_cell_outputs_changes(self, *args) -> None:
+    #     print(args)
 
-    def cell_id_to_index(self, cell_id: str) -> int:
-        """Find the index of the cell with the given ID.
+    def get_cell(self, cell_id: str) -> Map | None:
+        """Find the cell with the given ID.
 
-        If the cell cannot be found it will return -1.
+        If the cell cannot be found it will return ``None``.
 
         Args:
             cell_id: str
         Returns:
-            Cell index
+            Cell or None
         """
         for index, cell in enumerate(self):
             if cell["id"] == cell_id:
-                return index
+                return cast(Map, self._doc.ycells[index])
 
-        return -1
+        return None
 
-    def update_document(self, message_type: AIMessageType, message: str, cell_id: str = "") -> None:
+    def update_document(
+        self, prompt: dict, message_type: AIMessageType, message: str, cell_id: str = ""
+    ) -> None:
         """Update the document.
 
         Args:
+            prompt: User prompt
             message_type: Type of message to insert in the document
             message: Message to insert
             cell_id: Cell targeted by the update; if empty, the notebook is the target
         """
+        message_dict = {"parent_id": prompt["id"], "message": message, "type": message_type}
 
-    def notify(self, message: str, cell_id: str = "") -> None:
-        """Send a transient message to users.
+        def set_message(metadata: Map, message: dict):
+            if "datalayer" not in metadata:
+                metadata["datalayer"] = {"ai": {"prompts": [], "messages": []}}
+            elif "ai" not in metadata["datalayer"]:
+                metadata["datalayer"] = {"ai": {"prompts": [], "messages": []}}
+            elif "messages" not in metadata["datalayer"]["ai"]:
+                metadata["datalayer"]["ai"] = {"messages": []}
 
-        Args:
-            message: Notification message
-            cell_id: Cell targeted by the notification; if empty the notebook is the target
-        """
+            metadata["datalayer"]["ai"]["messages"].append(message)
+
+            metadata["datalayer"] = metadata["datalayer"].copy()
+
+        if cell_id:
+            cell = self.get_cell(cell_id)
+            if not cell:
+                raise ValueError(f"Cell [{cell_id}] not found.")
+            if "metadata" not in cell:
+                cell["metadata"] = Map({"datalayer": {"ai": {"prompts": [], "messages": []}}})
+            set_message(cell["metadata"], message_dict)
+
+        else:
+            notebook_metadata = self._doc._ymeta["metadata"]
+            set_message(notebook_metadata, message_dict)
+
+    # def notify(self, message: str, cell_id: str = "") -> None:
+    #     """Send a transient message to users.
+
+    #     Args:
+    #         message: Notification message
+    #         cell_id: Cell targeted by the notification; if empty the notebook is the target
+    #     """
