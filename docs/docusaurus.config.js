4523943089cae3146fa235e73d06405dfb69ed8f
Eric Charles <226720+echarles@users.noreply.github.com>
2025-08-14 12:05:12 +0200

    upgrade: env vars (#41)
    * upgrade: env vars
    
    * revert: azure env var

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -86,7 +86,7 @@ module.exports = {
           'aria-label': 'LinkedIn',
         },
         {
-          href: 'https://bsky.app/profile/datalayer.io',
+          href: 'https://bsky.app/profile/datalayer.ai',
           position: 'right',
           className: 'header-bluesky-link',
           'aria-label': 'Bluesky',
